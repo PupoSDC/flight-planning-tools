d8c3e650f1b175ffb9311fc24a65cc03fdfaf67c
Pedro Pupo Sa da Costa <pedro@hublo.com>
2023-01-10 19:25:21 +0100

    feat: start development calculator filler

---
+++ ebart/ifrCalculatorFiller.js
@@ -0,0 +1,42 @@
+const Airfield = ""
+const runway = ""
+const qnh = ""
+const windDir = ""
+const windVelocity = ""
+const weight = "" 
+
+const inputs = document
+    .querySelector(".ewa-ptp-description")
+    .parentElement
+    .querySelectorAll("input")
+
+
+inputs[0].value = "EPLL"
+inputs[1].value = "251"
+inputs[2].value = "2500"
+inputs[3].value = "2560"
+inputs[4].value = "2500"
+inputs[5].value = "2500"
+inputs[6].value = "606"
+inputs[7].value = "0"
+inputs[8].value = "PD"
+inputs[9].value = "3135"
+inputs[10].value = "632"
+inputs[11].value = "70"
+inputs[11].value = "5"
+inputs[12].value = "15"
+inputs[13].value = "1007"
+inputs[14].value = "100"
+inputs[15].value = "10"
+inputs[16].value = "12"
+inputs[17].value = "1187"
+inputs[18].value = "200"
+inputs[19].value = "1139"
+inputs[20].value = "7000"
+inputs[21].value = "1180"
+inputs[22].value = "16"
+inputs[23].value = "9"
+inputs[24].value = "6"
+inputs[25].value = "0"
+inputs[26].value = "4"
+
